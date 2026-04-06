93e5f7667630cb1d86a64def29675b57e0f3a9c2
EmmaWikstrom <emma.malmwikstrom@gmail.com>
2026-04-06 21:09:04 +0200

    feat: fetch and display yarn data on detail page

--- src/pages/YarnDetailsPage.jsx
+++ src/pages/YarnDetailsPage.jsx
@@ -1,12 +1,42 @@
 import { useParams } from "react-router-dom";
+import { use, useEffect, useState } from "react";   
 
 export function YarnDetailsPage() {
   const { id } = useParams();
 
+  const [yarn, setYarn] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchYarn = async () => {
+        try {
+            const response = await fetch(`https://knitting-api.onrender.com/api/yarns/${id}`);
+            const data = await response.json();
+            
+            setYarn({...data, id: data._id,});
+        } catch (error) {
+            console.error("Error fetching yarn details:", error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    fetchYarn();
+}, [id]);
+
+if (loading) {
+    return <p>Loading yarn details...</p>;
+}
+
+if (!yarn) {
+    return <p>Yarn not found.</p>;
+}
+
   return (
-    <main className="container">
-      <h1>Yarn details</h1>
-      <p>Details for yarn with id: {id}</p>
-    </main>
+  <main className="container">
+    <h2>{yarn.name}</h2>
+    <p>Brand: {yarn.brand}</p>
+    <p>Weight: {yarn.weight}</p>
+    <p>Fiber: {yarn.fiber}</p>
+  </main>
   );
 }
